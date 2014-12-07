59a66ade005bf79f5b20d2a95bd0008beb86b53b
Josh Pollock <jpollock412@gmail.com>
2014-12-07 16:27:55 -0500

    only public post types

--- src/templates/general-settings.php
+++ src/templates/general-settings.php
@@ -28,7 +28,7 @@
 			<label style="float:left;"><?php _e( 'Post Types', 'metaplate' ); ?></label>
 			<div style="margin-left: 180px; padding-top: 6px;">
 			<?php
-			$post_types = get_post_types(array(), 'objects');
+			$post_types = get_post_types( array( 'public' => true ), 'objects' );
 			foreach($post_types as $type){?>
 			<label style="display:block; margin-bottom:4px;"><input type="checkbox" data-live-sync="true" name="post_type[<?php echo $type->name; ?>]" value="1" {{#if post_type/<?php echo $type->name; ?>}}checked="checked"{{/if}}> <?php echo $type->label; ?></label>
 			<?php } ?>
