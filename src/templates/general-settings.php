c6efd4fe1042ea40628c3e1362b518fe09c94e42
Josh Pollock <jpollock412@gmail.com>
2014-12-08 19:03:56 -0500

    don't show content type specific options for non content type specific template

--- src/templates/general-settings.php
+++ src/templates/general-settings.php
@@ -6,35 +6,37 @@
 			<label><?php _e( 'Slug', 'metaplate' ); ?></label>
 			<input type="text" name="slug" value="{{slug}}" data-format="slug" data-sync=".metaplate-subline" data-master="#metaplate-name" id="metaplate-slug" required>
 		</div>
-		<div class="metaplate-config-group">
-			<label><?php _e( 'Page Types', 'metaplate' ); ?></label>
-			<select name="page_type" data-live-sync="true">
-				<option value="single" {{#is page_type value="single"}}selected="selected"{{/is}}><?php _e( 'Single', 'metaplate' ); ?></option>
-				<option value="archive" {{#is page_type value="archive"}}selected="selected"{{/is}}><?php _e( 'Archive (list)', 'metaplate' ); ?></option>
-				<option value="both" {{#is page_type value="both"}}selected="selected"{{/is}}><?php _e( 'Both', 'metaplate' ); ?></option>
-			</select>
-		</div>
-		<div class="metaplate-config-group">
-			<label><?php _e( 'Placement', 'metaplate' ); ?></label>
-			<select name="placement" data-live-sync="true">
-				<option value="append" {{#is placement value="append"}}selected="selected"{{/is}}><?php _e( 'After Content', 'metaplate' ); ?></option>
-				<option value="prepend" {{#is placement value="prepend"}}selected="selected"{{/is}}><?php _e( 'Before Content', 'metaplate' ); ?></option>
-				<option value="replace" {{#is placement value="replace"}}selected="selected"{{/is}}><?php _e( 'Replace Content', 'metaplate' ); ?></option>
-			</select>
-			{{#is placement value="replace"}}<p class="description"><?php _e( 'Use \{{content}} in the template to indicate placement of original content.', 'metaplate' ); ?></p>{{/is}}
-		</div>
 
-		<div class="metaplate-config-group">
-			<label style="float:left;"><?php _e( 'Post Types', 'metaplate' ); ?></label>
-			<div style="margin-left: 180px; padding-top: 6px;">
-			<?php
-			$post_types = get_post_types( array( 'public' => true ), 'objects' );
-			foreach($post_types as $type){?>
-			<label style="display:block; margin-bottom:4px;"><input type="checkbox" data-live-sync="true" name="post_type[<?php echo $type->name; ?>]" value="1" {{#if post_type/<?php echo $type->name; ?>}}checked="checked"{{/if}}> <?php echo $type->label; ?></label>
-			<?php } ?>
+		<?php if ( $metaplate[ 'content_type_specific' ]  ) { ?>
+			<div class="metaplate-config-group">
+				<label><?php _e( 'Page Types', 'metaplate' ); ?></label>
+				<select name="page_type" data-live-sync="true">
+					<option value="single" {{#is page_type value="single"}}selected="selected"{{/is}}><?php _e( 'Single', 'metaplate' ); ?></option>
+					<option value="archive" {{#is page_type value="archive"}}selected="selected"{{/is}}><?php _e( 'Archive (list)', 'metaplate' ); ?></option>
+					<option value="both" {{#is page_type value="both"}}selected="selected"{{/is}}><?php _e( 'Both', 'metaplate' ); ?></option>
+				</select>
 			</div>
-		</div>
+			<div class="metaplate-config-group">
+				<label><?php _e( 'Placement', 'metaplate' ); ?></label>
+				<select name="placement" data-live-sync="true">
+					<option value="append" {{#is placement value="append"}}selected="selected"{{/is}}><?php _e( 'After Content', 'metaplate' ); ?></option>
+					<option value="prepend" {{#is placement value="prepend"}}selected="selected"{{/is}}><?php _e( 'Before Content', 'metaplate' ); ?></option>
+					<option value="replace" {{#is placement value="replace"}}selected="selected"{{/is}}><?php _e( 'Replace Content', 'metaplate' ); ?></option>
+				</select>
+				{{#is placement value="replace"}}<p class="description"><?php _e( 'Use \{{content}} in the template to indicate placement of original content.', 'metaplate' ); ?></p>{{/is}}
+			</div>
 
+			<div class="metaplate-config-group">
+				<label style="float:left;"><?php _e( 'Post Types', 'metaplate' ); ?></label>
+				<div style="margin-left: 180px; padding-top: 6px;">
+				<?php
+				$post_types = get_post_types( array( 'public' => true ), 'objects' );
+				foreach($post_types as $type){?>
+				<label style="display:block; margin-bottom:4px;"><input type="checkbox" data-live-sync="true" name="post_type[<?php echo $type->name; ?>]" value="1" {{#if post_type/<?php echo $type->name; ?>}}checked="checked"{{/if}}> <?php echo $type->label; ?></label>
+				<?php } ?>
+				</div>
+			</div>
+		<?php } ?>
 
 
 		<?php
